1ba0c39cfc55e42f49a4507b3ad91a084ceb378e
kirin-phasuk <kirin0phasuk@gmail.com>
2026-07-23 11:56:50 +0700

    Update: UI PDF8.

--- src/App.jsx
+++ src/App.jsx
@@ -2128,9 +2128,10 @@ function PrintApplicationForm({ student }) {
          </div>
          {/*<div className="ml-auto mr-[40mm] font-bold text-sm">สก ๐๓</div>*/}
       </div>
-
-      <h2 className="font-bold text-center">ใบสมัครงาน</h2>
-      <h5 className="text-center">(Job application form)</h5>
+      <div className="space-y-8 text-sm leading-relaxed">
+        <h2 className="font-bold text-center">ใบสมัครงาน</h2>
+        <h5 className="text-center mt-8">(Job application form)</h5>
+      </div>
 
       {/* ส่วนข้อมูล */}
       <div className="space-y-6 text-sm leading-relaxed">
